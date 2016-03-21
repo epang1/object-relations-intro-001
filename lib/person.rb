fd271b2b64313eca00d14721d28748f2fb82e76f
epang1 <epang1@gmail.com>
2016-03-20 21:56:26 -0400

    done

--- lib/person.rb
+++ lib/person.rb
@@ -1,21 +1,42 @@
-#code along here!
+require 'pry'
+class Person
+ 
+  attr_accessor :name, :happiness, :partner
+ 
+  def initialize(name)
+    @name = name 
+    @happiness = 5
+  end
 
+  def go_on_a_date(person, match)
+    if match == true
+      self.happiness += 1
+      person.happiness += 1
+    else
+      self.happiness -= 1
+      person.happiness -= 1
+    end
+  end
 
-#############
+  def get_married(partner)
+    self.partner = partner
+    partner.partner = self
+  end
+end
 
-#When your tests pertaining to the first code along are passing, un-comment out the below!
+# When your tests pertaining to the first code along are passing, un-comment out the below!
 
-#beyonce = Person.new("Beyonce")
-#jayz = Person.new("Jay-Z")
+# beyonce = Person.new("Beyonce")
+# jayz = Person.new("Jay-Z")
 # beyonce.go_on_a_date(jayz, true)
 # puts beyonce.happiness
 # puts jayz.happiness
 
-#When your tests pertaining to the second code along are passing, un-comment out the below!
+# # When your tests pertaining to the second code along are passing, un-comment out the below!
 
-#beyonce.get_married(jayz)
-#puts "Beyonce's partner is #{beyonce.partner.name}"
-#puts "Jay-Z's partner is #{jayz.partner.name}"
+# beyonce.get_married(jayz)
+# puts "Beyonce's partner is #{beyonce.partner.name}"
+# puts "Jay-Z's partner is #{jayz.partner.name}"
 
 
 
